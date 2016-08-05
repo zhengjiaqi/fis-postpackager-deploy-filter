4d21a270cf14bad6f4ac379aee4c21cb35dafff6
jiaqi.zheng <jiaqi.zheng@qunar.com>
2016-08-05 09:29:21 +0800

    deploy filter

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = function(ret, conf, settings, opt) {
-  if(opt.dest == 'preview'){
+  if (opt.dest == 'preview') {
     return;
   }
+  var dependence = [];
   fis.util.map(ret.src, function(subpath, file) {
     var fileId = file.getId();
     var include = settings.include;
@@ -15,10 +16,23 @@ module.exports = function(ret, conf, settings, opt) {
         regexp.test(fileId) && (deploy = true);
       })
     }
-    if (!deploy) {
+    if (deploy) {
+      addDependence(file);
+    }
+    function addDependence(file) {
+      var requires = file.requires;
+      dependence.push(file.getId());
+      dependence = dependence.concat(requires);
+      requires.forEach(function(key ,index){
+        addDependence(ret.ids[key]);
+      });
+    }
+  });
+
+  fis.util.map(ret.src, function(subpath, file) {
+    if (dependence.indexOf(file.getId()) < 0) {
       file.requires = [];
       file.release = false;
     }
   });
-
 };
